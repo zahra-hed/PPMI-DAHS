48278bbdc64253eea5f8b6a3e918c81c5bee862b
Riasok <125545213+Riasok@users.noreply.github.com>
2023-03-08 23:58:48 +0900

    Update prepcode.R

--- preprocessing/prepcode.R
+++ preprocessing/prepcode.R
@@ -1,36 +1,12 @@
-##DATA
-BJLO <- read.csv(file="C://Users//kosai//Desktop//Project PP//Parkinson Features//Benton_Judgement_of_Line_Orientation.csv") #Benton Judgement of line Orientation
-HVLT <- read.csv(file="C://Users//kosai//Desktop//Project PP//Parkinson Features//Hopkins_Verbal_Learning_Test_-_Revised.csv") #Hopkins Verbal Learning Test_revised
-SDMT <- read.csv(file="C://Users//kosai//Desktop//Project PP//Parkinson Features//Symbol_Digit_Modalities_Test.csv") #Symbol Digit Modalities Test
-MSF <- read.csv(file="C://Users//kosai//Desktop//Project PP//Parkinson Features//Modified_Semantic_Fluency.csv") #Modified Semantic Fleuncy
-MOCA <- read.csv(file="C://Users//kosai//Desktop//Project PP//Parkinson Features//Montreal_Cognitive_Assessment__MoCA_.csv") #Montreal_Cognitive Assesment
-GDS <- read.csv(file="C://Users//kosai//Desktop//Project PP//Parkinson Features//Geriatric_Depression_Scale__Short_Version_.csv") #Geriatic Depression Scale
-STAI <- read.csv(file="C://Users//kosai//Desktop//Project PP//Parkinson Features//State-Trait_Anxiety_Inventory.csv") #State-trait Anxiety Inventory
-QUIP <- read.csv(file="C://Users//kosai//Desktop//Project PP//Parkinson Features//QUIP-Current-Short.csv") #QUIP
-ESS <- read.csv(file="C://Users//kosai//Desktop//Project PP//Parkinson Features//Epworth_Sleepiness_Scale.csv") #Epworth Sleepiness Scale
-SCOPA <- read.csv(file="C://Users//kosai//Desktop//Project PP//Parkinson Features//SCOPA-AUT.csv") #SCOPA-AUT
-M1 <- read.csv(file="C://Users//kosai//Desktop//Project PP//Parkinson Features//MDS-UPDRS_Part_I.csv")
-M1Q <- read.csv(file="C://Users//kosai//Desktop//Project PP//Parkinson Features//MDS-UPDRS_Part_I_Patient_Questionnaire.csv")
-M2 <- read.csv(file="C://Users//kosai//Desktop//Project PP//Parkinson Features//MDS_UPDRS_Part_II__Patient_Questionnaire.csv")
-M3 <- read.csv(file="C://Users//kosai//Desktop//Project PP//Parkinson Features//MDS_UPDRS_Part_III.csv")
-M4 <- read.csv(file="C://Users//kosai//Desktop//Project PP//Parkinson Features//MDS-UPDRS_Part_IV__Motor_Complications.csv")
-DTSC <- read.csv(file="C://Users//kosai//Desktop//Project PP//Parkinson Features//DaTScan_Analysis.csv")
-LNS <- read.csv(file="C://Users//kosai//Desktop//Project PP//Parkinson Features//Letter_-_Number_Sequencing.csv")
-UPSIT <- read.csv(file="C://Users//kosai//Desktop//Project PP//Parkinson Features//Letter_-_Number_Sequencing.csv")
-RBD <- read.csv(file="C://Users//kosai//Desktop//Project PP//Parkinson Features//REM_Sleep_Behavior_Disorder_Questionnaire.csv")
-MCI <- read.csv(file="C://Users//kosai//Desktop//Project PP//Parkinson Features//Cognitive_Categorization.csv")
-UPSIT <- read.csv(file="C://Users//kosai//Desktop//Project PP//Parkinson Features//University_of_Pennsylvania_Smell_Identification_Test__UPSIT_.csv")
+install.packages('dplyr')
+install.packages('tidyverse')
 
+library("dplyr")
+library('tidyverse')
 
-demographic <- read.csv(file="C://Users//kosai//Desktop//Project PP//Parkinson Features//Demographics.csv")
-Status <- read.csv(file="C://Users//kosai//Desktop//Project PP//Parkinson Features//Participant_Status.csv")
-socio_economics <-read.csv(file="C://Users//kosai//Desktop//Project PP//Parkinson Features//Socio-Economics.csv")
-death_a <- read.csv(file="C://Users//kosai//Desktop//Project PP//Parkinson Features//Conclusion_of_Study_Participation-Archived.csv")
-death <- read.csv(file="C://Users//kosai//Desktop//Project PP//Parkinson Features//Conclusion_of_Study_Participation.csv")
-
-
-
-##Pre processing---------------------
+setwd("C://Users//kosai//Desktop//PPMI//features")
+(temp = list.files(pattern="*.csv"))
+myfiles = lapply(temp, read.delim)
 preprocessing <- function(inputdata){
   inputdata <- inputdata %>% group_by(PATNO) %>% arrange(DATE, .by_group=TRUE)
   ##RECID creation col : NO
@@ -99,55 +75,55 @@ preprocessing <- function(inputdata){
   inputdata <- inputdata %>% filter(DATE_I != 0)
   return(inputdata)
 }
+for (i in 1:length(temp)) assign(temp[i], preprocessing(read.csv(temp[i]))) ; temp <- NULL; myfiles <- NULL
 
-preprocessing(BJLO) -> BJLO
-preprocessing(ESS) -> ESS
-preprocessing(GDS) -> GDS
-preprocessing(HVLT) -> HVLT
-preprocessing(M1) -> M1
-preprocessing(M1Q) -> M1Q
-preprocessing(M2) -> M2
-preprocessing(M3) -> M3
-preprocessing(M4) -> M4
-preprocessing(MOCA) -> MOCA
-preprocessing(MSF) -> MSF
-preprocessing(QUIP) -> QUIP
-preprocessing(SCOPA) -> SCOPA
-preprocessing(DTSC) -> DTSC
-preprocessing(LNS) -> LNS
-preprocessing(RBD) -> RBD
-preprocessing(MCI) -> MCI
-preprocessing(UPSIT) -> UPSIT
-preprocessing(SDMT) -> SDMT
-preprocessing(STAI) -> STAI
+total_features <- full_join(data.frame(KEY = as.vector(BJLO.csv$KEY), BJLO = as.vector(BJLO.csv$JLO_TOTRAW)),
+                       data.frame(KEY = as.vector(ESS.csv$KEY), ESS = as.vector(ESS.csv$TOTAL)),
+                       data.frame(KEY = as.vector(GDS.csv$KEY), GDS = as.vector(GDS.csv$GDS_SUM)),
+                       data.frame(KEY = as.vector(HVLT.csv$KEY), HBLT = as.vector(HVLT.csv$DVT_TOTAL_RECALL)),
+                       data.frame(KEY = as.vector(LNS.csv$KEY), LNS = as.vector(LNS.csv$LNS_TOTRAW)),
+                       data.frame(KEY = as.vector(M1.csv$KEY), M1 = as.vector(M1.csv$NP1RTOT)),
+                       data.frame(KEY = as.vector(M1Q.csv$KEY), M1Q = as.vector(M1Q.csv$NP1PTOT)),
+                       data.frame(KEY = as.vector(M2.csv$KEY), M2 = as.vector(M2.csv$NP2PTOT)),
+                       data.frame(KEY = as.vector(M3.csv$KEY), M3 = as.vector(M3.csv$NP3TOT)),
+                       data.frame(KEY = as.vector(M4.csv$KEY), M4 = as.vector(M4.csv$NP4TOT)),
+                       data.frame(KEY = as.vector(MCI.csv$KEY), MCI = as.vector(MCI.csv$COGSTATE)),
+                       data.frame(KEY = as.vector(MOCA.csv$KEY), MOCA = as.vector(MOCA.csv$MCATOT)),
+                       data.frame(KEY = as.vector(MSF.csv$KEY), MSF = as.vector(MSF.csv$DVT_SFTANIM)),
+                       data.frame(KEY = as.vector(QUIP.csv$KEY), QUIP = as.vector(QUIP.csv$QUIP_SUM)),
+                       data.frame(KEY = as.vector(RBD.csv$KEY), RBD = as.vector(RBD.csv$sum)),
+                       data.frame(KEY = as.vector(SDMT.csv$KEY), SDMT = as.vector(SDMT.csv$SDMTOTAL)),
+                       data.frame(KEY = as.vector(STAI.csv$KEY), STAI = as.vector(STAI.csv$STAI_SUM)),
+                       data.frame(KEY = as.vector(SCOPA-AUT.csv$KEY), SCOPA = as.vector(SCOPA-AUT.csv$sum))
+)
+df <- total_features %>% reduce(full_join, by = "KEY") ; total_features <- NULL
 
-##Creating Total Data set ------------------------------------------------
-KEY <- c(0); total <- data.frame(KEY)
-for (i in 1:14){
-  data.frame(Total_Variables[i]) -> variable
-  temp <- data.frame(variable$KEY, variable$score); names(temp) <- c("KEY", variable); total <- full_join(total, temp, by ="KEY")
+df[,"PATNO"] <- c(0); df[,"DATE"] <- c(0)
+for (i in 1:length(df$KEY)){
+  df$PATNO[i] = as.integer(floor(df$KEY[i]/10000))
+  df$DATE[i] = as.integer(df$KEY[i] %% 10000)
+}
+df$REC_ID <- c(0); preprocessing(df) -> df; df$REC_ID <- NULL
+df$year <- c(0); temp = 0
+
+for (i in 1:length(df$PATNO)){
+  temp = temp + df$DATE_I[i]
+  if (df$NUM[i] == 1){
+    temp = 0
+  }
+  df$year[i] = (temp %/% 100)+1
 }
 
+df <- df %>%
+  group_by(PATNO, year) %>%                # class별로 분리
+  summarise(BJLO = mean(BJLO), HVLT = mean(HVLT), SDMT = mean(SDMT), MSF = mean(MSF), MCI = mean(MCI),
+            MOCA = mean(MOCA), GDS = mean(GDS), STAI = mean(STAI), QUIP = mean(QUIP), ESS = mean(ESS), RBD = mean(RBD), LNS = mean(LNS),
+            SCOPA = mean(SCOPA_AUT), M1 = mean(M1), M2 = mean(M2), M3 = mean(M3), M4 = mean(M4), PATNO = mean(PATNO), 
+            DATE= min(DATE), DATE2 = min(DATE2)
+)
 
-##HARD CODING ------
-temp<- data.frame(BJLO$KEY, BJLO$JLO_TOTRAW); names(temp)<-c("KEY","BJLO"); total<-temp
-temp<- data.frame(HVLT$DVT_TOTAL_RECALL, HVLT$KEY); names(temp)<-c("HVLT","KEY"); total<-full_join(total,temp, by="KEY")
-temp<- data.frame(SDMT$SDMTOTAL, SDMT$KEY); names(temp)<-c("SDMT","KEY"); total<-full_join(total,temp, by="KEY")
-temp<- data.frame(MSF$DVT_SFTANIM, MSF$KEY); names(temp)<-c("MSF","KEY"); total<-full_join(total,temp, by="KEY")
-temp<- data.frame(MOCA$MCATOT, MOCA$KEY); names(temp)<-c("MOCA","KEY"); total<-full_join(total,temp, by="KEY")
-temp<- data.frame(GDS$GDS_SUM, GDS$KEY); names(temp)<-c("GDS","KEY"); total<-full_join(total,temp, by="KEY")
-temp<- data.frame(STAI$STAI_SUM, STAI$KEY); names(temp)<-c("STAI","KEY"); total<-full_join(total,temp, by="KEY")
-temp<- data.frame(QUIP$QUIP_SUM, QUIP$KEY); names(temp)<-c("QUIP","KEY"); total<-full_join(total,temp, by="KEY")
-temp<- data.frame(ESS$TOTAL, ESS$KEY); names(temp)<-c("ESS","KEY"); total<-full_join(total,temp, by="KEY")
-temp<- data.frame(SCOPA$SUM, SCOPA$KEY);names(temp)<-c("SCOPA","KEY"); total<-full_join(total,temp, by="KEY")
-temp<- data.frame(M1$NP1RTOT, M1$KEY); names(temp)<-c("M1","KEY"); total<-full_join(total,temp, by="KEY")
-temp<- data.frame(M1Q$NP1PTOT, M1Q$KEY); names(temp)<-c("M1Q","KEY"); total<-full_join(total,temp, by="KEY")
-temp<- data.frame(M2$NP2PTOT, M2$KEY); names(temp)<-c("M2","KEY"); total<-full_join(total,temp, by="KEY")
-temp<- data.frame(M3$NP3TOT, M3$KEY); names(temp)<-c("M3","KEY"); total<-full_join(total,temp, by="KEY")
-temp<- data.frame(M4$NP4TOT, M4$KEY); names(temp)<-c("M4","KEY"); total<-full_join(total,temp, by="KEY")
-temp<- data.frame(LNS$LNS_TOTRAW, LNS$KEY); names(temp)<-c("LNS","KEY"); total<-full_join(total,temp, by="KEY")
-temp<- data.frame(RBD$sum, RBD$KEY); names(temp)<-c("RBD","KEY"); total<-full_join(total,temp, by="KEY")
-temp<- data.frame(DTSC$PUT, DTSC$CAUD, DTSC$KEY); names(temp)<-c("PUT", "CAUD","KEY"); total<-full_join(total,temp, by="KEY")
-temp<- data.frame(MCI$COGSTATE, MCI$KEY); names(temp)<-c("MCI","KEY"); total<-full_join(total,temp, by="KEY")
-#temp<- data.frame(UPSIT$TOTAL_CORRECT, UPSIT$KEY); names(temp)<-c("UPSIT","KEY"); total<-full_join(total,temp, by="KEY")
 
+
+##YEAR CUT
+##CONCOHRT CUT
+total <- total |> filter(CONCOHORT == 1)
